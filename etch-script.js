7378989aa5915eece2384dd0dc714dc2de92b3cd
“JustinVx” <justin.venbrux@gmail.com>
2016-10-17 00:20:08 +0200

    Changed the look of the page.

--- etch-script.js
+++ etch-script.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function() {
   $('.title-message').html("<h1>Ready to start!</h1>");
+  var black_blocks = 0;
 
   function create_blocks(amount, row_length) {
     var count = 0;
@@ -11,18 +12,20 @@ jQuery(document).ready(function() {
     $('.grid').css("width", row_length*20 + "px")
   }
 
+  function count_black(){
+    black_blocks++;
+    $('.title-message').html("<h1>Amount of black blocks: " + black_blocks + "</h1>");
+  }
+
   $('button').click(function(){
     $('.title-message').html("<h1>Grid created!</h1>");
     $(this).remove();
     create_blocks(400, 20);
   })
 
-  /*$('.block').hover(function(){
-    $('.block').css("background-color", "black");
-  })*/
-
   $(document).on('mouseenter', '.block', function() {
     $(this).css("background-color", "black");
+    count_black();
   });
 
 })
