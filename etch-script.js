23e9315c8b92908d0e8a9f4055988eee59652ce4
“JustinVx” <justin.venbrux@gmail.com>
2016-10-18 16:28:22 +0200

    Made grid width not longer depending on amount of blocks.

--- etch-script.js
+++ etch-script.js
@@ -5,11 +5,14 @@ jQuery(document).ready(function() {
   function create_blocks(height_b, width_b) {
     var count = 0;
     var amount = height_b * width_b;
+    var width_of_a_single_block = 320 / width_b;
     for (i = 0; i < amount; i++){
       $('.grid').append("<div class=\"block\"></div>");
       count++;
     }
-    $('.grid').css("width", width_blocks*20 + "px")
+    $('.block').css("width", width_of_a_single_block + "px");
+    $('.block').css("height", width_of_a_single_block + "px");
+    $('.grid').css("width", 320 + "px");
   }
 
   function count_black(){
@@ -21,16 +24,16 @@ jQuery(document).ready(function() {
     $('.title-message').html("<h1>Grid created!</h1>");
     var height_blocks = $('#heigth_blocks').val();
     var width_blocks = $('#width_blocks').val();
-    $('.title-message').html("<h1>Test: " + width_blocks + "</h1>");
-    $(this).remove();
-    $('.input-fields').remove();
-    //height_blocks = parseInt(height_blocks);
-    create_blocks(height_blocks, height_blocks);
-
+    //$('.title-message').html("<h1>Test: " + width_blocks + "</h1>");
+    $(this).hide();
+    $('.input-fields').hide();
+    //werk nu niet: if (!heigth_blocks) {heigth_blocks = 12;}
+    // werkt nu niet: if (!width_blocks) {width_blocks = 12;}
+    create_blocks(height_blocks, width_blocks);
   })
 
   $(document).on('mouseenter', '.block', function() {
-    $(this).css("background-color", "black");
+    $(this).css("background-color", "#DB504A");
     count_black();
   });
 
