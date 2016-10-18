8cc8e088e674d5c2f6714160df801efc6d5c15a9
“JustinVx” <justin.venbrux@gmail.com>
2016-10-18 20:47:15 +0200

    Added possibility to pick a color. But height and width of grid now broken.

--- etch-script.js
+++ etch-script.js
@@ -1,39 +1,53 @@
 jQuery(document).ready(function() {
   $('.title-message').html("<h1>Ready to start!</h1>");
+  var current_color = "#DB504A"
 
   function create_blocks(height_b, width_b) {
-    var count = 0;
+    $('.block').remove();
     if (height_b > 100) {heigth_b = 100;}
     if (width_b > 100) {width_b = 100;}
     var amount = height_b * width_b;
+
     var width_of_a_single_block = 320 / width_b;
     for (i = 0; i < amount; i++){
       $('.grid').append("<div class=\"block\"></div>");
-      count++;
+      $('.title-message').html(i);
     }
     $('.block').css("width", width_of_a_single_block + "px");
     $('.block').css("height", width_of_a_single_block + "px");
     $('.grid').css("width", 320 + "px");
   }
 
+  create_blocks(12, 12);
+
+  setInterval(function(){
+    var random_color = 'rgb(' + (Math.floor((256)*Math.random())) + ',' + (Math.floor((256)*Math.random())) + ',' + (Math.floor((256)*Math.random())) + ')';
+    $('#rainbow').css("background-color", random_color);
+  },1000);
+
   $('button').click(function(){
     $('.title-message').html("<h1>Grid created!</h1>");
-    var height_blocks = $('#heigth_blocks').val();
-    var width_blocks = $('#width_blocks').val();
+    //var height_blocks = $('#heigth_blocks').val();
+    //var width_blocks = $('#width_blocks').val();
     //$('.title-message').html("<h1>Test: " + width_blocks + "</h1>");
-    $(this).hide();
     $('.input-fields').hide();
     // werkt nu niet: if (!heigth_blocks) {heigth_blocks = 12;}
     // werkt nu niet: if (!width_blocks) {width_blocks = 12;}
-    create_blocks(height_blocks, width_blocks);
+    create_blocks(12, 12);
+  })
+  
+  $('.color').click(function(){
+    current_color = $(this).css("background-color");
+    $('.color').css("border-radius", "50%");
+    $(this).css("border-radius", "0%");
   })
 
-  // toevoegen knoppen waar gekozen kan worden voor kleur om te kleuren.
-
   $(document).on('mouseenter', '.block', function() {
-    // toevoegen switch statement voor knoppen per kleur.
-    var hue = 'rgb(' + (Math.floor((256)*Math.random())) + ',' + (Math.floor((256)*Math.random())) + ',' + (Math.floor((256)*Math.random())) + ')';
-    $(this).css("background-color", hue);
+    if (current_color === "rgb(0, 0, 0)"){
+      $('.title-message').html("<h1>BLACK</h1>");
+      current_color = 'rgb(' + (Math.floor((256)*Math.random())) + ',' + (Math.floor((256)*Math.random())) + ',' + (Math.floor((256)*Math.random())) + ')';
+    }
+    $(this).css("background-color", current_color);
   });
 
 })
