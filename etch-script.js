c750275bf88843afc802c0c65e107a26ebea650b
“JustinVx” <justin.venbrux@gmail.com>
2016-10-18 16:50:26 +0200

    Randomised colours.

--- etch-script.js
+++ etch-script.js
@@ -1,9 +1,10 @@
 jQuery(document).ready(function() {
   $('.title-message').html("<h1>Ready to start!</h1>");
-  var black_blocks = 0;
 
   function create_blocks(height_b, width_b) {
     var count = 0;
+    if (height_b > 100) {heigth_b = 100;}
+    if (width_b > 100) {width_b = 100;}
     var amount = height_b * width_b;
     var width_of_a_single_block = 320 / width_b;
     for (i = 0; i < amount; i++){
@@ -15,11 +16,6 @@ jQuery(document).ready(function() {
     $('.grid').css("width", 320 + "px");
   }
 
-  function count_black(){
-    black_blocks++;
-    $('.title-message').html("<h1>Amount of black blocks: " + black_blocks + "</h1>");
-  }
-
   $('button').click(function(){
     $('.title-message').html("<h1>Grid created!</h1>");
     var height_blocks = $('#heigth_blocks').val();
@@ -27,14 +23,17 @@ jQuery(document).ready(function() {
     //$('.title-message').html("<h1>Test: " + width_blocks + "</h1>");
     $(this).hide();
     $('.input-fields').hide();
-    //werk nu niet: if (!heigth_blocks) {heigth_blocks = 12;}
+    // werkt nu niet: if (!heigth_blocks) {heigth_blocks = 12;}
     // werkt nu niet: if (!width_blocks) {width_blocks = 12;}
     create_blocks(height_blocks, width_blocks);
   })
 
+  // toevoegen knoppen waar gekozen kan worden voor kleur om te kleuren.
+
   $(document).on('mouseenter', '.block', function() {
-    $(this).css("background-color", "#DB504A");
-    count_black();
+    // toevoegen switch statement voor knoppen per kleur.
+    var hue = 'rgb(' + (Math.floor((256)*Math.random())) + ',' + (Math.floor((256)*Math.random())) + ',' + (Math.floor((256)*Math.random())) + ')';
+    $(this).css("background-color", hue);
   });
 
 })
